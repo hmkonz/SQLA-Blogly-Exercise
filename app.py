1427164810b55ea5310191eb3722111accdc25e9
Heather Konz <hmkonz@yahoo.com>
2023-01-18 10:55:50 -0700

    changes to app.py and added new template

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ debug = DebugToolbarExtension(app)
 
 
 connect_db(app)
-db.create_all()
+
 
 app.app_context().push()
 
@@ -68,9 +68,9 @@ def show_details(user_id):
 
 @app.route('/users/<int:user_id>/edit', methods = ['GET', 'POST'])
 def edit_user(user_id):
-    """Handle form submission for updating and existing user"""
+    """Handle form submission for updating an existing user"""
 
-    # if 'edit user form' has modifications to be saved and sent along with request, do the following; else return to details page if cancel or update the user
+    # if 'edit user form' has modifications to be saved and sent along with request, do the following; else return to details page if cancel 
   
     if request.method == 'POST': 
         
@@ -89,7 +89,7 @@ def edit_user(user_id):
         # in python, use an'f string' with a dynamic user_id so can be redirected to the corresponding user detail page
         return redirect(f'/users/{user_id}')
     else:
-        """As a GET request, show form to edit an existing user"""
+        """As a GET request, show form to edit an existing user """
         user=User.query.get_or_404(user_id)
         return render_template('edit_user.html', user=user)
 
@@ -109,17 +109,62 @@ def delete_user(user_id):
 def create_post(user_id):
     """Handle form submission for creating a new post"""
 
+    # retrieve data of user that's adding a post using their user_id 
+    user=User.query.get_or_404(user_id)
     # if form has inputs to send along with request do the following; else go back to create_post.html template
     if request.method == 'POST':
        # retrieve inputs from form 
         title=request.form['title']
         content=request.form['content']
 
-        new_post=Post(title=title, content=content)
+        new_post=Post(title=title, content=content, user=user)
         db.session.add(new_post)
         db.session.commit()
         return redirect(f'/users/{user_id}')
     else:
         """# As a GET request, show the form for creating a new post"""
-        user=User.query.get_or_404(user_id)
+        
         return render_template('create_post.html', user=user)
+
+@app.route('/posts/<post_id>')
+def show_post_details(post_id):
+    """Show a page with details about a specific post"""
+    user=User.query.get_or_404(post_id)
+    post=Post.query.get_or_404(post_id)
+    return render_template('post_details.html', user=user, post=post)
+
+@app.route('/posts/<post_id>/edit', methods=["GET", "POST"])
+def edit_post(post_id):
+    """Handle form submission for updating an existing post"""
+
+    # if 'edit post form' has modifications to be saved and sent along with request, do the following; else return to details page if cancel 
+  
+    if request.method == 'POST': 
+        
+    
+        # retrieve data of user editing their user info using their user_id 
+        updated_post=User.query.get_or_404(post_id)
+        
+        # retrieve edited inputs from form and update first_name, last_name and image_url in the table
+        updated_post.title=request.form['title']
+        updated_post.content=request.form['content']
+        
+        db.session.add(updated_post)
+        db.session.commit()
+        # in python, use an'f string' with a dynamic post_id so can be redirected to the corresponding post detail page
+        return redirect(f'/posts/{post_id}')
+    else:
+        """As a GET request, show form to edit an existing post"""
+        post=User.query.get_or_404(post_id)
+        return render_template('edit_post.html', post=post)
+
+@app.route('/posts/int:<post_id>/delete', methods = ['POST'])
+def delete_post(user_id, post_id):
+    """Handle form submission for deleting an existing user and showing updated list of users"""
+
+    # retrieve data of post to be deleted using the post_id 
+    delete_post=User.query.get_or_404(post_id)
+    # delete post
+    db.session.delete(delete_post)
+    db.session.commit()
+    return redirect ('/users/{user_id}')
